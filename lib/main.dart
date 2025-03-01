40ec58a9024c866d2eba233f52283cd3bf5a0cc6
Adnan Khan <jackmarkiplier@gmail.com>
2025-03-01 14:53:53 -0500

    First Widget called MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,18 @@ class Task {
 
   Task({required this.name, this.isCompleted = false});
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TaskListScreen(),
+    );
+  }
+}
